7da71f5222f93b31f0bda6346317e5ea7c2ec50c
Wellington Garcia <wellington.garcia@wisereducacao.com>
2020-09-12 01:19:37 -0300

    remove cache

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,11 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+type Props = {
+  title: string;
+};
+
+export default function Home({ title }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -11,11 +15,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!{title} </a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -56,10 +60,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
