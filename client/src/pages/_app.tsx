4ddbe80b903857f82a87c523e9c4df88e64bdcaf
Wellington Garcia <wellington.garcia@wisereducacao.com>
2020-09-13 12:44:22 -0300

    pwa

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -7,7 +7,9 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>React Avançado</title>
-        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="description" content="A simple project" />
       </Head>
       <GlobalStyles />
