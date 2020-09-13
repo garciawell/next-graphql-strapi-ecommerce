9391ab4cf738f8c69fd345fc2fc4fa3751610817
Wellington Garcia <wellington.garcia@wisereducacao.com>
2020-09-13 14:18:37 -0300

    strapi configuration

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f7d56293566966a19d4ec22088c5dda5'),
+    },
+  },
+});
