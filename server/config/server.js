b950ff2908e50c0ffb1514238ca7ed44b4c67fa3
Wellington Garcia <wellington.garcia@wisereducacao.com>
2020-09-13 17:28:13 -0300

    teste 2

--- server/config/server.js
+++ server/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f7d56293566966a19d4ec22088c5dda5'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
