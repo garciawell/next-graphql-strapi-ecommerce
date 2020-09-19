25bbaae655c6dea057abbf17d890097e64054a0e
Wellington Garcia <wellington.garcia@wisereducacao.com>
2020-09-18 23:36:53 -0300

    New project

---
+++ client/src/components/Button/index.tsx
@@ -0,0 +1,28 @@
+import { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react'
+import * as S from './styles'
+
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>
+
+export type ButtonProps = {
+  size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
+  icon?: JSX.Element
+  as?: React.ElementType
+} & ButtonTypes
+
+const Button = ({
+  children,
+  icon,
+  size = 'medium',
+  fullWidth = false,
+  ...props
+}: ButtonProps) => (
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+    {icon}
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
+)
+
+export default Button
