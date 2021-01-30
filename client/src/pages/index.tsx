ba4f3b37b0416e529e0f50dab522973c22b065b3
Wellington Garcia <garciawell@gmail.com>
2021-01-30 01:07:18 -0300

    feat: new project

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,51 @@
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
+
+export default function Index(props: HomeTemplateProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache()
+  })
+
+  client.query({
+    query: gql`
+      query getGames {
+        games {
+          name
+        }
+      }
+    `
+  })
+
+  return <Home {...props} />
+}
+
+// ATENÇÃO:
+// os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
+
+// getStaticProps => gerar estático em build time
+// getServerSideProps => gerar via ssr a cada request
+// getInitialProps => gerar via ssr a cada request
+export function getServerSideProps() {
+  // faz lógica
+  // pode ser buscar dados numa API
+  // fazer calculo|leitura de context
+
+  // retorno dos dados
+  return {
+    props: {
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcomingGames: gamesMock,
+      upcomingHighlight: highlightMock,
+      upcomingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighlight: highlightMock
+    }
+  }
+}
