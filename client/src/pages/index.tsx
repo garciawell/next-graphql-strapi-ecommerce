3f4fe3e3742199efced38fb9bd6715b7c43d4d85
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-01-30 01:16:03 -0300

    Apolo client

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,24 +1,22 @@
-import Home, { HomeTemplateProps } from 'templates/Home'
+import { gql, useQuery } from '@apollo/client'
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
-import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
+import Home, { HomeTemplateProps } from 'templates/Home'
 
 export default function Index(props: HomeTemplateProps) {
-  const client = new ApolloClient({
-    uri: 'http://localhost:1337/graphql',
-    cache: new InMemoryCache()
-  })
-
-  client.query({
-    query: gql`
-      query getGames {
-        games {
-          name
-        }
+  const { data, loading, error } = useQuery(gql`
+    query getGames {
+      games {
+        name
       }
-    `
-  })
+    }
+  `)
+
+  if (loading) return <p>loading...</p>
+  if (error) return <p>{error}</p>
+
+  if (data) return <p>{JSON.stringify(data, null, 2)}</p>
 
   return <Home {...props} />
 }
