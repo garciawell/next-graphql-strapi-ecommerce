a8f445a26ddf2357c2933182d819e7828713235d
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-01-30 01:47:10 -0300

    query graphql

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,21 +1,9 @@
-import { gql, useQuery } from '@apollo/client'
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 import Home, { HomeTemplateProps } from 'templates/Home'
-import { initializeApollo } from 'utils/apollo'
-
-const GET_GAMES = gql`
-  query getGames {
-    games {
-      name
-    }
-  }
-`
 
 export default function Index(props: HomeTemplateProps) {
-  if (props.data) return <p>{JSON.stringify(props.data, null, 2)}</p>
-
   return <Home {...props} />
 }
 
@@ -26,14 +14,8 @@ export default function Index(props: HomeTemplateProps) {
 // getServerSideProps => gerar via ssr a cada request
 // getInitialProps => gerar via ssr a cada request
 export async function getServerSideProps() {
-  const apolloClient = initializeApollo()
-
-  const { data } = await apolloClient.query({ query: GET_GAMES })
-
   return {
     props: {
-      data,
-      initialApolloState: apolloClient.cache.extract(),
       banners: bannersMock,
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
